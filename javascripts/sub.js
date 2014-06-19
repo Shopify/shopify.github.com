4a51e5c92287639e6268c03d4993b0f032a87d1a
Carson Shold <carson@carsonshold.com>
2014-06-19 12:01:27 -0400

    Global open source file

---
+++ javascripts/sub.js
@@ -0,0 +1,34 @@
+var shopifyOpenSource = (function(module, $) {
+
+  var settings;
+
+  init = function (options) {
+
+    // Default settings
+    settings = {
+      repo_name: null
+    };
+
+    // Override defaults with arguments
+    $.extend(settings, options);
+
+    // Remove links from images inside links
+    $('a:has(img)').addClass("nounderline");
+  };
+
+  getStats = function () {
+    var uri = 'https://api.github.com/repos/' + settings.repo_name + '?callback=?';
+
+    $.getJSON(uri, function(result) {
+      if (result.meta.status == 403) {
+        // hit the request limit. damn.
+        document.getElementById('starCount').innerHTML = '!';
+        document.getElementById('forkCount').innerHTML = '!';
+      } else {
+        document.getElementById('starCount').innerHTML = result.data.stargazers_count;
+        document.getElementById('forkCount').innerHTML = result.data.forks_count;
+      }
+    });
+  }
+
+}(shopifyOpenSource || {}, jQuery));
