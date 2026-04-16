b6e15705a7cfd76594b5a0d8cca76ada552bafc8
James Lepp <jlepp@ieee.org>
2026-04-16 13:22:18 -0400

    removing dead files and code

--- javascripts/shopify-opensource.js
+++ javascripts/shopify-opensource.js
@@ -1,20 +1,3 @@
-// Fix for .indexOf in IE8 and below
-if (!Array.prototype.indexOf) {
-  Array.prototype.indexOf = function(elt /*, from*/) {
-    var len = this.length >>> 0,
-        from = Number(arguments[1]) || 0;
-    from = (from < 0) ? Math.ceil(from) : Math.floor(from);
-    if (from < 0)
-      from += len;
-
-    for (; from < len; from++) {
-      if (from in this && this[from] === elt)
-        return from;
-    }
-    return -1;
-  };
-}
-
 window.SHOPIFYTIMBER = window.SHOPIFYTIMBER || {};
 
 jQuery(function($){
